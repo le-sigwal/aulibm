e26484d38266869c436d61c8c8f895b93a3e3131
Le.Sigwal <61907278+le-sigwal@users.noreply.github.com>
2025-07-14 22:12:51 +0200

    Compilation issue over gwin64-7.3.0 (mingw32 host)
    https://al.liammig.bzh/aulibm/ftq/#AUM_VR_000001
    
    - mingw32 host was not supported
    - configure parameters were not used for mingw

--- src/mathlib_config.h
+++ src/mathlib_config.h
@@ -9,10 +9,17 @@
 #   include "mathlib_config_msvc.h"
 //
 #elif defined (__MINGW32__) /* MinGW */
-# ifndef _MATHLIB_DLL_
-#   define _MATHLIB_DLL_
-# endif
-# include "mathlib_config_mingw.h"
+#  if defined __has_include /* MinGW/gcc/autoconf */
+#    if __has_include ("mathlib_configuration.h")
+#      include "mathlib_configuration.h"
+#    endif
+#  endif
+#  ifndef _MATHLIB_DLL_
+#    define _MATHLIB_DLL_
+#  endif
+#  ifndef MATHLIB_MINGW /* MinGW/gcc/bat */
+#    include "mathlib_config_mingw.h"
+#  endif
 #else /* Other: presumably Linux/gcc/autoconf */
 # ifndef _MATHLIB_DLL_
 #   define _MATHLIB_DLL_
